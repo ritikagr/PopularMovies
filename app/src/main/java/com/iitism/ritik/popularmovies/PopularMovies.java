dac25bc6c7a282a7597889d1124bfb8892582ffe
Ritik Kumar Agrahari <ritikagrespace@gmail.com>
2017-01-28 16:10:33 +0530

    Update PopularMovies.java

--- app/src/main/java/com/iitism/ritik/popularmovies/PopularMovies.java
+++ app/src/main/java/com/iitism/ritik/popularmovies/PopularMovies.java
@@ -11,7 +11,7 @@ import io.realm.Realm;
  */
 public class PopularMovies extends Application{
 
-    public static final String Key = "1fc12ca5d33e28912325b3188ac062e8";
+    public static final String Key = "add API key here";
     public static final String BASE_URL = "https://api.themoviedb.org/3/movie/";
     public static final String API_KEY = "?api_key=" + Key;
     public static final String LANG_PAGE = "&language=en-US&page=";
